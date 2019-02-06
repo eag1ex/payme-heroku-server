4af66dc37ad97dc140b502fa5cbe0dbed9243fc2
eag1ex <eaglex@eaglex.net>
2019-02-06 21:45:30 +0700

    update newapi

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ Object.assign = require('object-assign');
 var express = require('express'),
   app = express(),
   morgan = require('morgan');
-   
+const {cloneDeep} = require('lodash')
+const _= require('lodash')      
 var routerApp = express.Router();
 
 var fs = require('fs');
@@ -25,7 +26,6 @@ var path = require('path');
 var newsApi = require('./lib/newsapi-api')
 
 var q = require('q');
-var _ = require('lodash');
 var filters = require('./lib/filters.js');
 var appRouter = require('./lib/app.router');
 var Ctrl = require('./lib/ctrls.js');
@@ -61,6 +61,7 @@ var port = config.port
 
 
 
+var newsEverithingCache = [];
 //////////////////////////////////////////
 /////////// authenticate all requests
 // POST: /authenticate
@@ -76,8 +77,8 @@ app.get("/spike-api/news/headlines/:search", function(req, res) {
       return res.status(200).json(resp);
     } 
   }) 
-
 });
+
 app.get("/spike-api/news/everything/:search", function(req, res) {
   var search = req.params.search || 'all';
   if(search==='all')search='nasdaq'
@@ -86,9 +87,67 @@ app.get("/spike-api/news/everything/:search", function(req, res) {
     if(resp.error){
       return res.status(400).json(resp);
     }else{
+      if(resp.articles){
+        var articles = resp.articles;
+
+        if(newsEverithingCache.length>20){
+          newsEverithingCache = [];
+        }
+        const _newsEverithingCache = cloneDeep(newsEverithingCache);
+        articles = _newsEverithingCache.concat(articles);
+        resp.articles = articles;
+      }
       return res.status(200).json(resp);
     } 
   }) 
+});
+
+
+app.post("/spike-api/news/everything", function(req, res) {
+ 
+  const body = req.body || {}
+  
+  if(!body.description || !body.rating){
+    return res.status(400).json({error:true,message:'wrong values provided'});
+  }
+
+  if ((body.description  || '').length > 500) {
+    return res.status(400).json({error:true,message:'Message too long'});
+  }
+
+	if ((body.description || '').length < 6) {
+    return res.status(400).json({error:true,message:'Message too short'});
+  }
+  if (!validationForSpecialchar(body.description)) {
+    return res.status(400).json({error:true,message:'Illegal characters'});
+  }
+
+  const newItem = {
+    rating:body.rating,
+    author:'john doe',
+    title:'n/a',
+    description:body.description,
+    source:{id:'n/a', name:'n/a'},
+    content:'n/a',
+    publishedAt:new Date().getTime(),
+    urlToImage:'',
+    url:''
+  } 
+    newsEverithingCache.unshift(newItem);
+    return res.status(200).json(newItem);
+ 
+
+  function validationForSpecialchar(str) {
+    const bad = `[^*_|~={}[]:;<>@#]`.toLowerCase(); /// i know its stupid
+    let invalid = false;
+    for (let i = 0; i < bad.length; i++) {
+      if (str.toLowerCase().indexOf(bad[i]) !== -1) {
+        invalid = true;
+        break;
+      }
+    }
+    return !invalid;
+  }
 });
 
 
